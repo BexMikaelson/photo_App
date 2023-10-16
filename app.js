2a69f47064a1a779c6a235f5d1fcfa958ab2619a
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-10-16 19:02:01 +0200

    router config

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const app = require('express')();
-const PORT = 3000;
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.APP_PORT;
+const userRouter = require('./api/users/user.router');
+const albumsRouter = require('./api/albums/albums.router');
+app.use(express.json());
 
-app.get('/api', (req, res) => {
-    res.json({
-        success: 1,
-        message: "Rest api is working properly"
-    });
-});
+app.use('/api/users', userRouter);
+app.use('/api/albums', albumsRouter);
+
+
+
+ /* app.get('/api', (req, res) => {
+     res.json({
+         success: 1,
+         message: "Rest api is working properly"
+     });
+ }); */
 
 app.listen(PORT, () => {
-    console.log('Server up and running, listening on port 3000');
+    console.log('Server up and running, listening on port', process.env.APP_PORT );
 })
