15b6d57503154e3e73de9ca6641d0886a91e512f
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-10-17 20:14:19 +0200

    added photo router

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 const PORT = process.env.APP_PORT;
 const userRouter = require('./api/users/user.router');
 const albumsRouter = require('./api/albums/albums.router');
+const photosRouter = require('./api/photos/photos.router');
 app.use(express.json());
 
 app.use('/api/users', userRouter);
 app.use('/api/albums', albumsRouter);
+app.use('/api/photos', photosRouter);
 
 
 
