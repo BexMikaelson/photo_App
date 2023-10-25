24e13329325b8a399dc98783cdc63b0a799eac1a
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-10-25 12:31:54 +0200

    removed checkToken for getUserByUserId

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -6,11 +6,10 @@ const {
 } = require('./user.controller');
 
 const router = require('express').Router();
-const { checkToken } = require('../../auth/token_validation')
 
 router.post('/register', createUser);
 router.get('/', getUsers);
-router.get('/:id', checkToken, getUserByUserId);
+router.get('/:id', getUserByUserId);
 router.post('/login', login);
 
 module.exports = router;
