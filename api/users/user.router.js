d5d72b85b63201c655b7ee37dec8dba563bff334
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-10-25 11:31:29 +0200

    removed checkToken from getUsers

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -9,7 +9,7 @@ const router = require('express').Router();
 const { checkToken } = require('../../auth/token_validation')
 
 router.post('/register', createUser);
-router.get('/', checkToken, getUsers);
+router.get('/', getUsers);
 router.get('/:id', checkToken, getUserByUserId);
 router.post('/login', login);
 
